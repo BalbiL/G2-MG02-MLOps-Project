8d145e3d76dca44972de6807ce0c688fd77e463e
Georges HOUMBADJI <georges.houmbadji@edu.ece.fr>
2025-11-16 17:36:19 +0100

    feat(ml): A first architecture defined for the Two-Tower model (without optimization)

--- frontend/app.py
+++ frontend/app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 # 2. (Conditional). If User ID is in db, go from 1 to 3
 #                   Otherwise, insert User ID into db and display the page for selecting topics of interest.
-# The goal of this page is to propose topics to the user depending on thoses in the MIND dataset (sport, etc.)
+# The goal of this page is to propose topics to the user depending on those in the MIND dataset (sport, etc.)
 
 # 3. Recommendations page.
 #     => Not personalized / First recommendations (5 "news" items  + 15 items based on their choices on the second page).
@@ -15,3 +15,4 @@ import streamlit as st
 
 # Note : A recommendation can be materialized by a kind of card with a title and when clicked, the abstract appears.
 # The card should use the pydantic model returned by the API
+# Icons, categories, etc.
