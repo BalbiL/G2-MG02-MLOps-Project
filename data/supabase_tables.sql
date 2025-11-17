3b8324ceb093130eae003d4b69d1d4a386f2a9a6
Elise <148560661+EliseBrn@users.noreply.github.com>
2025-11-17 12:03:23 +0100

    Fix foreign key references in SQL tables

--- data/supabase_tables.sql
+++ data/supabase_tables.sql
@@ -1,6 +1,5 @@
 -- SQL used for supabase --
 -- Already in the section "SQL Editor" in Supabase --
-
 -- Users --
 
 create table if not exists user_topics (
@@ -12,14 +11,14 @@ create table if not exists user_topics (
 -- Recommandations --
 
 create table if not exists default_recs (
-    news_id text references news(news_id),
+    news_id text references news(id),
     score float,
     generation_time timestamptz default now()
 );
 
 create table if not exists personalized_recs (
     user_id uuid references auth.users(id),
-    news_id text references news(news_id),
+    news_id text references news(id),
     score float,
     generation_time timestamptz default now(),
     primary key (user_id, news_id)
@@ -31,6 +30,6 @@ create table if not exists personalized_recs (
 create extension if not exists vector;
 
 create table if not exists news_vec (
-    news_id text primary key references news(news_id),
+    news_id text primary key references news(id),
     vec vector(768)  -- selon le modèle d'embedding
 );
